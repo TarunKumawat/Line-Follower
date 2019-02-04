dce4b7f50ea904c83a826ada6d3bbd43e279bb60
TarunKumawat <46136273+TarunKumawat@users.noreply.github.com>
2019-02-04 18:06:29 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,96 +1,186 @@
 /*
- * line follower with pid implementation.cpp
+ * hashtag electrical.cpp
  *
- * Created: 03-02-2019 10:26:08
+ * Created: 16-11-2018 21:22:59
  * Author : HP PC
  */ 
 
-
-#include <avr/io.h>
 #include <stdio.h>
+#include <util/delay.h>
+#include <avr/io.h>
 #include <avr/sfr_defs.h>
 
-float kp=1;
-float kd=1;
-float left_rpm,right_rpm;
-float currentvalue,desiredvalue;
-float p_error,t_error,ferror,maxerror=50;
-float d_error,error=0,error0,ferror,bv=300;
-int a,h=0,pin_s[8]={0};
-int pin_w[8]={2,4,6,8,10,12,14,16};
+void forward();
+void left();
+void right();
+void hardleft();
+void hardright();
+void stop();
+int prev=0;
 
-void pwm_initializing(int n)
+void pwm_init()
 {
-	TCRR0 |=(1<<WGM00)|(1<<WGM01);
-	TCCR0 |=(1<<CS01);
+	TCCR0 |=(1<<WGM00) | (1<<WGM01) ;
+	TCCR0 |=(1<<COM00) | (1<<COM01) ;
+	TCCR0 |=(1<<CS01)
+	TCCR0 &= ~(1<<CS02)
 	ICR1=1000;
-}
-int forward(float left_rpm, float right_rpm)
-{
-	PORTD=0b00001010;
-	OCR1=left_rpm;
-	OCR2=right_rpm;
-}
-
-                                                                                                         
-int errorcalci(int e)
-{
-	p_error=t_error;
-	for(a=0;a<8;a++)
-	{
-		if(pin_s[a]==1)
-		h++;
-		if(h>0)
-		{
-		error+= pin_s[a]*pin_w[a];
-		error\=h;
-		}
-		
-	}
-	
-	if(h==0)
-	error=maxerror;
-	t_error=kp*error+kd*error;
-	
-	d_error=t_error-p_error;
-	
-	ferror= kp*t_error+kd*d_error;
-	
-	return ferror ;
 	
 }
 
+void forward()
+{
+	PORTD=0b00010100;
+	prev=3;
+	OCR1A=1000;
+	OCR1B=1000;
+}
+
+void left()
+{
+	PORTD &= ~(1<<PIND4);
+	prev=2;
+	OCR1A=450;
+	OCR1B=450;
+}
+void right()
+{
+	PORTD &= ~(1<<PIND2);
+	OCR1A=450;
+	OCR1B=450;
+	prev=4;
+}
+
+ void hardleft()
+ {
+ 	PORTD &= ~(1<<PIND4);
+	PORTD |= (1<<PIND5);
+	OCR1A=750;
+	OCR1B=750;
+	prev=1;
+ }
+
+ void hardright()
+ {
+	 PORTD &= ~(1<<PIND2);
+	 PORTD |= (1<<PIND3);
+	 OCR1A=750;
+	 OCR1B=750;
+ 	prev=5;
+ } 
+
+void stop()
+{
+	PORTD &= (5<<PIND2);
+	prev=6;
+}
 
 int main(void)
 {
+    /* Replace with your application code */
 
 	
-    /* Replace with your application code */
-    while (1) 
+	DDRD=0b11111111;
+	DDRA=0b00000000;
+	
+	
+	PORTD=0b00000000;
+	
+//	forward();
+    while (1) ;
+	
+	
     {
-		DDRD=0b11111111;
-		DDRA=0xFF;
+	
+	 
+	if (	bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
+	{
+		if(prev==1)
+		{
+			right();
+		}
+		else if(prev==2)
+		{
+			right();
+		}
+		else if(prev==3)
+		{
+			right();
+		}
+			else if(prev==4)
+			{
+				left();
+			}
+			else if(prev==5)
+			{
+			left();	
+			}
+			
+			else
+			{
+				stop();
+			}
+			
+		}
 		
-		float rv,lv;
-	error0=	errorcalci();
-	if(error0==9)
+	else if(bit_is_set(PINA,0)&&bit_is_set(PINA,1)&&bit_is_set(PINA,2)&&bit_is_set(PINA,3)&&bit_is_set(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
 	{
-	forward(bv,bv);	
+		stop();
 	}
-	else if(error0>9)
+	
+	else if(	bit_is_clear(PINA,0)&&bit_is_set(PINA,1)&&bit_is_set(PINA,2)&&bit_is_set(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
 	{
-	forward(bv+error,bv-error);
+		left();
+	}	
+	else if(	bit_is_set(PINA,0)&&bit_is_set(PINA,1)&&bit_is_set(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
+	{
+		left();
 	}
-	else(error0<9)
+	else if(	bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_set(PINA,2)&&bit_is_set(PINA,3)&&bit_is_set(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
 	{
-	forward(bv-error,bv+error);	
+		forward();
+	}
+	else if(	bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_set(PINA,3)&&bit_is_set(PINA,4)&&bit_is_set(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
+	{
+		forward();
+	}
+	
+	else if( bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_set(PINA,5)&&bit_is_set(PINA,6)&&bit_is_set(PINA,7))
+	{
+		
+		right();
+	}
+	else if( bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_set(PINA,4)&&bit_is_set(PINA,5)&&bit_is_set(PINA,6)&&bit_is_clear(PINA,7))
+	{
+		
+		right();
+	}
+	
+	else if( bit_is_set(PINA,0)&&bit_is_set(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
+	{
+	hardleft();	
+	}
+	else if( bit_is_set(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_clear(PINA,7))
+	{
+		hardleft();
+	}
+	
+	else if( bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_set(PINA,6)&&bit_is_set(PINA,7))
+	{
+		hardright();
+	}
+	else if( bit_is_clear(PINA,0)&&bit_is_clear(PINA,1)&&bit_is_clear(PINA,2)&&bit_is_clear(PINA,3)&&bit_is_clear(PINA,4)&&bit_is_clear(PINA,5)&&bit_is_clear(PINA,6)&&bit_is_set(PINA,7))
+	{
+		hardright();
+	}
+	
+	
+	}
 	}
 	
 	
 	
 		
-		
-		
-    }
-}
+	
+    
 
